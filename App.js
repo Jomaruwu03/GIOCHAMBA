3f88a891fd219690f92641c8381a7d8a4e910072
Jomaruwu03 <jomar.jaae@gmail.com>
2024-01-17 01:50:47 -0500

    Tarea1

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [isPressed, setIsPressed] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const handlePressIn = () => {
+    setIsPressed(true);
+  };
+
+  const handlePressOut = () => {
+    setIsPressed(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: darkMode ? '#121212' : '#fff' }]}>
+      <Text style={{ color: darkMode ? '#fff' : '#000' }}>Hola mundo, por parte del sm54</Text>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          {
+            backgroundColor: darkMode ? (isPressed ? '#333' : '#555') : isPressed ? '#ccc' : '#ddd',
+            borderColor: darkMode ? '#fff' : '#000',
+          },
+        ]}
+        onPress={toggleDarkMode}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+      >
+        <Text style={{ color: darkMode ? '#fff' : '#000' }}>
+          Cambiar a {darkMode ? 'Modo Aleman' : 'Modo Dano'}
+        </Text>
+      </TouchableOpacity>
+      <StatusBar style={darkMode ? 'light' : 'dark'} />
     </View>
   );
 }
@@ -13,8 +44,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 1,
   },
 });
